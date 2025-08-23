a914b0fc13a657cf50bd0e4bf20ee98caeefd66d
DREADANGEL777 <dreadangel27@gmail.com>
2025-08-23 14:41:12 +0300

    final commit second try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import "./Home.css"
+import placeHolder from '../assets/placeholder.webp'
 
 const API_KEY = import.meta.env.VITE_API_KEY
 
